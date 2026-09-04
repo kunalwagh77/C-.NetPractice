8cb0a298594946a4374aef4b41d9f5b148e6a11e
Kunal Wagh <kunalwagh901@gmail.com>
2026-09-04 14:42:56 +0530

    Add new C# practice

---
+++ StringBuilderRealLifeExmaple/StringBuilderRealLifeExmaple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // StringBuilder object create
+        StringBuilder studentInfo = new StringBuilder();
+
+        // Student information add kar rahe hain
+        studentInfo.AppendLine("----- STUDENT INFORMATION -----");
+
+        studentInfo.AppendLine("Name: Kunal Wagh");
+        studentInfo.AppendLine("Course: MCA");
+        studentInfo.AppendLine("Technology: .NET");
+
+        studentInfo.AppendLine("-------------------------------");
+
+        // Complete information print
+        Console.WriteLine(studentInfo);
+    }
+}
